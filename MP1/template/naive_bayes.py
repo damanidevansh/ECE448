cd0cad4178196f8877a00ed2cb3974e04239aca6
Devansh Damani <85512173+damanidevansh@users.noreply.github.com>
2023-09-11 23:23:11 -0500

    Update naive_bayes.py

--- MP1/template/naive_bayes.py
+++ MP1/template/naive_bayes.py
@@ -21,6 +21,7 @@ import reader
 import math
 from tqdm import tqdm
 from collections import Counter
+import numpy as np
 
 
 '''
@@ -47,11 +48,65 @@ Main function for training and predicting with naive bayes.
     You can modify the default values for the Laplace smoothing parameter and the prior for the positive label.
     Notice that we may pass in specific values for these parameters during our testing.
 """
-def naiveBayes(dev_set, train_set, train_labels, laplace=1.0, pos_prior=0.5, silently=False):
+def naiveBayes(dev_set, train_set, train_labels, laplace=0.01, pos_prior=0.8, silently=False):
     print_values(laplace,pos_prior)
 
     yhats = []
-    for doc in tqdm(dev_set, disable=silently):
-        yhats.append(-1)
-
+    
+    neg_prior = 1-pos_prior
+    
+    known_pos, unknown_pos = training(train_set, train_labels, laplace, 1)
+    known_neg, unknown_neg = training(train_set, train_labels, laplace, 0)
+    
+    for ind_set in dev_set:
+        prob_negwords = 0
+        prob_poswords = 0
+        
+        prob_poswords += np.log(pos_prior)
+        prob_negwords += np.log(neg_prior)
+        
+        for words in ind_set:
+            if words in known_pos:
+                prob_poswords += np.log(known_pos[words])
+            else:
+                prob_poswords += np.log(unknown_pos)
+        
+            if words in known_neg:
+                prob_negwords += np.log(known_neg[words])
+            else:
+                prob_negwords += np.log(unknown_neg)
+        
+        if prob_poswords >+ prob_negwords:
+            yhats.append(1)
+        else:
+            yhats.append(0)
     return yhats
+
+
+def training (train_set, train_labels, laplace_smoothing, type):
+    nlabels = len(train_labels)
+    word_list = {}
+    known_prob = {}
+    unknown_prob = 0
+    nwords = 0
+    ntypes = 0
+    
+    for i in range(nlabels):
+        if (train_labels[i] == type):
+            for training_word in train_set[i]:
+                if training_word in word_list:
+                    word_list[training_word] += 1
+                else:
+                    word_list[training_word] += 1
+    
+    ntype = len(word_list)
+    
+    for i in word_list:
+        nwords += word_list[i]
+        
+    for i in word_list:
+        known_prob[i] = (word_list[i] + laplace_smoothing)/(nwords + (laplace_smoothing * (ntype+1)))
+        
+        unknown_prob = laplace_smoothing/(nwords + (laplace_smoothing*(ntypes + 1)))
+    
+    return known_prob, unknown_prob
